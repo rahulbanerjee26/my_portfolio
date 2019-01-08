8fd7695eb5de18185b185e0ea54f9f2333cb46c9
Rahul Banerjee <40443639+rahulbanerjee26@users.noreply.github.com>
2019-01-08 12:30:49 -0400

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import {Grid,Cell} from 'react-mdl';
-import avatar from "./images/avatar.jpg"
+import aboutme from "./images/aboutme.jpg";
 
 class contact extends Component{
   render() {
     const image={
-      'margin-left': "20%",
+      'margin-left': "4%",
       'margin-top': "2%",
-       width: "40%",
+      'width': "90%",
       'border-radius': "50%"
     };
 
@@ -16,13 +16,13 @@ class contact extends Component{
       <div clasName ="contact-page">
       <Grid clasName="contact">
         <Cell col={6}>
-          <h1> RAHUL BANERJEE </h1>
+          <h1 clasName="contactheader"> RAHUL BANERJEE </h1>
           <img
-            style={image} src={avatar} alt="Logo"
+            style={image} src={aboutme} alt="Logo"
           />
         </Cell>
         <Cell col={6}>
-          <h1> CONTACT ME </h1>
+          <h1 clasName="contactheader"> CONTACT ME </h1>
           <div className="contact-icons">
             <i className="fa fa-phone-square"> (+1)647 913 8321</i>
             <i className="fa fa-envelope"> rahul.uoft@gmail.com</i>
